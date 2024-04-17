69baa834a7b875c80efa098495a36d57567a1b6e
james <wang.james.life@gmail.com>
2024-04-17 10:59:25 -0700

    first commit

---
+++ Taskademic/TaskademicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskademicApp.swift
+//  Taskademic
+//
+//  Created by James Wang on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskademicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
